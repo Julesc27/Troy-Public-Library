8f45bbc0e9199221ef5833c986b16fe2bc8e456c
mangopanda5 <pancham.boss008@gmail.com>
2026-02-13 12:44:58 -0500

    Adding calendar

--- Prototype (meredith)/resources/tpl.js
+++ Prototype (meredith)/resources/tpl.js
@@ -1,4 +1,42 @@
 
+// handles the library hours
+
+let today = new Date();
+
+
+/*
+  This function accepts a date and an announcement. If you do not have an announcement leave the announcement section blank, like this --> ""
+*/
+
+setHours(today, "");
+
+function setHours(date, announcement){
+
+  const hours = document.getElementById('libraryHours');
+  //if there is a closing announcement or changed hours announcement the message will be displayed
+  if(announcement != ""){
+    hours.textContent = announcement;
+  }
+  //Monday - Thursday Hours
+  else if(date.getDay() >= 1 && date.getDay() <= 4 ){
+    hours.textContent = "Hours: 9am - 6pm";
+  }
+  //Friday Hours
+  else if(date.getDay() == 5){
+    hours.textContent = "Hours: 9am - 5pm";
+  }
+  //Saturday
+  else if(date.getDay() == 6){
+    hours.textContent = "Hours: 10am - 5pm";
+  }
+  //Sunday
+  else{
+    hours.textContent = "Hours: Closed";
+  }
+}
+
+
+// handles the slideshow banner on the homepage
 let index = 0;
 let autoplayTimer = null;
 const autoplayDelay = 7000;
